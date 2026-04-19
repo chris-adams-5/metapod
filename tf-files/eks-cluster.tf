ed4c49c5a7e53c6ce43d05409921d7531003efd1
rahmat <rahmat@MASTAFF-Unassigned-Device-Q1Y0W3MJ60.local>
2026-04-19 18:57:35 +0100

    upgraded the terraform file

--- tf-files/eks-cluster.tf
+++ tf-files/eks-cluster.tf
@@ -1,44 +1,39 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "17.24.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
   cluster_name    = local.cluster_name
-  cluster_version = "1.29"
-  subnets         = data.aws_subnets.vpc_subnets.ids
+  cluster_version = "1.30"
+
+  subnet_ids = data.aws_subnets.vpc_subnets.ids
+  vpc_id     = aws_default_vpc.default_vpc.id
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    group_1 = {
+      name           = "worker-group-1"
+      instance_types = ["t2.small"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      
+
+      vpc_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    }
+
+    group_2 = {
+      name           = "worker-group-2"
+      instance_types = ["t2.medium"] # t2.medium is getting old!
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+      
+      vpc_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+    }
+  }
 
   tags = {
     Environment = "training"
   }
-
-  vpc_id = aws_default_vpc.default_vpc.id
-
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
-
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      public_ip = true
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-      public_ip = true
-    },
-  ]
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
 }
